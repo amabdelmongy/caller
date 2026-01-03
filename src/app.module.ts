06757eea3e1e4c9e454a5e98cbe5f9ae787c83c8
Ahmed Mongy <amabdelmongy@gmail.com>
2026-01-03 22:35:26 +0100

    add nestjs/serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,21 @@ import { CallerModule } from './caller/caller.module';
 import { LogsController } from './caller/logs.controller';
 import { HealthController } from './health/health.controller';
 
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import swaggerUiDist from 'swagger-ui-dist';
+
 @Module({
-  imports: [CallerModule],
+  imports: [
+    CallerModule,
+
+    // Serve Swagger UI assets so /swagger/swagger-ui*.js|css resolve in prod
+    ServeStaticModule.forRoot({
+      rootPath: join(swaggerUiDist.getAbsoluteFSPath()),
+      serveRoot: '/swagger',
+      exclude: ['/swagger', '/swagger/'],
+    }),
+  ],
   controllers: [LogsController, HealthController],
 })
 export class AppModule {}
